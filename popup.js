c51a6c145a9885d35789c82f36336a6c06ab75dc
Guils09 <Gui.a.carvalho@hotmail.com>
2026-01-03 13:08:50 +0000

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,15 @@
+document.getElementById("gmail").addEventListener("click", () => {
+  window.open("https://mail.google.com", "_blank");
+});
+
+document.getElementById("hackclub").addEventListener("click", () => {
+  window.open("https://hackclub.com", "_blank");
+});
+
+document.getElementById("hacktime").addEventListener("click", () => {
+  window.open("https://hackatime.hackclub.com", "_blank");
+});
+
+document.getElementById("flavortown").addEventListener("click", () => {
+  window.open("https://flavortown.hackclub.com/kitchen", "_blank");
+});
